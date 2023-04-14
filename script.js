61e4d97dd2fd910cd87da8fe6d27567a48c284cd
AlekseyPopov1993 <alekseypopov02061993@gmail.com>
2023-04-14 22:50:57 +0300

    add big slider

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(".mini-slider").slick({
 
     // normal options...
-    infinite: false,
+    infinite: true,
     slidesToShow: 6,
     slidesToScroll: 1,
     // loop: true,
     nextArrow: '<img src="pict01/right.png">',
     prevArrow: '<img src="pict01/left.png">',
+ 
   
     // the magic
     responsive: [{
@@ -47,4 +48,57 @@ $(".mini-slider").slick({
         settings: "unslick" // destroys slick
   
       }]
+  });
+
+
+
+  $(".big-slider").slick({
+
+    // normal options...
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // loop: true,
+    nextArrow: '<img src="pict01/right.png">',
+    prevArrow: '<img src="pict01/left.png">',
+  
+    // the magic
+    // responsive: [{
+  
+    //     breakpoint: 1300,
+    //     settings: {
+    //       slidesToShow: 4,
+    //       infinite: true
+    //     }
+  
+    //   },{
+  
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       infinite: true
+    //     }
+  
+    //   }, {
+  
+    //     breakpoint: 700,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       infinite: true
+    //     }
+  
+    //   },{
+  
+    //     breakpoint: 500,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       dots: true
+    //     }
+  
+    //   }, {
+  
+    //     breakpoint: 300,
+    //     settings: "unslick" // destroys slick
+  
+    //   }]
   });
