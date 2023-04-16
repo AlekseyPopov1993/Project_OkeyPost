d73dea601ddaaf05b08530d727a6a874b4a00ee1
AlekseyPopov1993 <alekseypopov02061993@gmail.com>
2023-04-16 15:42:48 +0300

    new header

--- script.js
+++ script.js
@@ -62,43 +62,14 @@ $(".mini-slider").slick({
     nextArrow: '<img src="pict01/right.png">',
     prevArrow: '<img src="pict01/left.png">',
   
-    // the magic
-    // responsive: [{
-  
-    //     breakpoint: 1300,
-    //     settings: {
-    //       slidesToShow: 4,
-    //       infinite: true
-    //     }
-  
-    //   },{
-  
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       infinite: true
-    //     }
-  
-    //   }, {
-  
-    //     breakpoint: 700,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       infinite: true
-    //     }
-  
-    //   },{
-  
-    //     breakpoint: 500,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       dots: true
-    //     }
-  
-    //   }, {
-  
-    //     breakpoint: 300,
-    //     settings: "unslick" // destroys slick
-  
-    //   }]
   });
+
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
